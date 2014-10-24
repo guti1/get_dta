d94547f943235b88ffe370e86cf1c5ab5ef79f0c
guti1 <guti1@freemail.hu>
2014-10-24 12:12:50 +0200

    0.3

--- proj1.R
+++ proj1.R
@@ -1,3 +1,7 @@
+#loading packages
+
+library(dplyr)
+
 
 #downloading and loading the dataset
 
@@ -21,17 +25,34 @@ Y_train <- read.table("UCI HAR Dataset/train/Y_train.txt", quote="\"")
 X_test <- read.table("UCI HAR Dataset/test/X_test.txt", quote="\"")
 Y_test <- read.table("UCI HAR Dataset/test/Y_test.txt", quote="\"")
 
+labels <- read.table("UCI HAR Dataset/activity_labels.txt", quote="\"")
+
 variables_x <- read.table("UCI HAR Dataset/features.txt", quote="\"")
 
-#setting variable names
+#setting variable names and labels
 
 names(X_train) <- variables_x$V2
 names(X_test) <- variables_x$V2
 
 
+
 #merging
 X_merged <- rbind(X_train, X_test)
 Y_merged <- rbind(Y_train, Y_test)
+
+
+
+data <- cbind(y)
+
+
+#Extracting mean and std dev for each measurements
+
+extract_features <- grepl("mean|std", features[,2], ignore.case=T)
+
+X_merged2 <- X_merged[, extract_features]
+
+
+
 
 
 
